12f8de4824beaadc94d8fbf9f6c9885735d9c9f0
Joe Quattrone <joequattrone5@gmail.com>
2018-10-11 18:24:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,3 +54,22 @@ document.addEventListener('keydown', function(e) {
   }
 }) */
 
+
+
+function moveDodgerRight() {
+ var leftNumbers = dodger.style.left.replace('px', " ")
+ var left = parseInt(leftNumbers, 10)
+
+	if (left > 0) {
+	  dodger.style.left = `${left + 1}px`
+	}   
+}
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerRight()
+  }
+})
+
+
