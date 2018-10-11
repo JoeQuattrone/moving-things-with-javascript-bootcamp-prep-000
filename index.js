5e382322e0f7a0b8ae1a7537031c7924e3a8187a
Joe Quattrone <joequattrone5@gmail.com>
2018-10-11 19:00:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ document.addEventListener('keydown', function(e) {
   var right = parseInt(rightNumbers, 10)
   
   if (right > 0) {
-  dodger.style.left =`${right - 1}px`
+  dodger.style.left =`${right + 1}px`
   }
 }
 
