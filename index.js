4f33ae27ec34bcbe7bb2d927cc1180a5610f83c5
Joe Quattrone <joequattrone5@gmail.com>
2018-10-11 18:05:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ function moveDodgerRight(e) {
   var rightNumbers = dodger.style.right.replace('px', ' ')
   var right = parseInt(rightNumbers, 10)
   
+  dodger.style.right =`${right - 1}px`
+  
 }
 
 
